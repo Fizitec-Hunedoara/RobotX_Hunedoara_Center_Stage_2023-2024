b996109c157c9ea324c87b46b89c4c06a56e2a00
RobotX <robotxbp@gmail.com>
2023-09-23 12:24:12 +0300

    declared the sliders and the spinning motors in the program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewJavaFile.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewJavaFile.java
@@ -7,6 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 @TeleOp
 public class NewJavaFile extends OpMode {
     public DcMotorEx motorBL,motorBR,motorFR,motorFL;
+    public DcMotorEx sliderL,sliderR,rotitor_sus,rotitor_jos;
     @Override
     public void init() {
 
